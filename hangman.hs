7c6d825d702e2c82d3f7f392f0b6d73aba8e614d
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-02 23:31:34 +0000

    Add function that displays a given string with a space after each character

--- hangman.hs
+++ hangman.hs
@@ -1,3 +1,7 @@
+import Data.List
+
+displayWordSpaced :: String -> String
+displayWordSpaced word = intersperse ' ' word
 
 checkGuessedChar :: Char -> String -> Bool
 checkGuessedChar guess word = guess `elem` word
