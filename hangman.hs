ba0b7144f0f9d3994b55a3faa3fed8a0e87165ab
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-02 23:47:10 +0000

    Add functions that display the instances of a given character in a word

--- hangman.hs
+++ hangman.hs
@@ -5,3 +5,12 @@ displayWordSpaced word = intersperse ' ' word
 
 checkGuessedChar :: Char -> String -> Bool
 checkGuessedChar guess word = guess `elem` word
+
+showCharInWord :: Char -> String -> String
+showCharInWord guess word = map compareCharsPartiallyApplied word
+    where compareCharsPartiallyApplied = compareChars guess
+
+compareChars :: Char -> Char -> Char
+compareChars char1 char2
+    |char1 == char2 = char1
+    |otherwise      = '_'
