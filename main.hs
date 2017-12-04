2eb554ed6eae2013a25c523e6b8623763a81dc75
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-04 03:46:14 +0000

    Edit main.hs to display the chosen word but with all of its characters hidden (replaced by underscores)

--- main.hs
+++ main.hs
@@ -3,4 +3,6 @@ import Hangman
 main = do
     putStrLn "Please enter the word to be guessed"
     word <- getLine
-    putStrLn ("Your chosen word is: " ++ word)
+    let wordCharsReplacedWithUnderscores = displayWordSpaced (showCharInWord word '_')
+    putStrLn ("Here is the chosen word with all of its characters hidden: ")
+    putStrLn wordCharsReplacedWithUnderscores
