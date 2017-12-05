466f563aaeb67a13f3f8166aac6566ea22d3895c
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-05 21:38:34 +0000

    Edit main.hs to use 'let' to define a block of IO that can then be executed repeatedly by using sequence

--- main.hs
+++ main.hs
@@ -6,14 +6,19 @@ main = do
     let wordCharsReplacedWithUnderscores = displayWordSpaced (showCharInWord word '_')
     putStrLn ("Here is the chosen word with all of its characters hidden: ")
     putStrLn wordCharsReplacedWithUnderscores
-    putStrLn "Please enter your guess for a letter in the chosen word:"
-    char <- getChar
-    let isCharInWord = checkGuessedChar char word
-    if(isCharInWord)
-        then do
-            putStrLn "You have guessed a letter correctly!"
-            putStrLn "Here is the word with the correctly guessed letter(s) revealed:"
-            let wordWithRevealedLetters = showCharInWord word char
-            putStrLn wordWithRevealedLetters
-        else
-            putStrLn "You have guessed incorrectly, sorry!"
+    let guessAChar = (do
+        putStrLn "Please enter your guess for a letter in the chosen word:"
+        char <- getChar
+        print char
+        let isCharInWord = checkGuessedChar char word
+        if(isCharInWord)
+            then do
+                putStrLn "You have guessed a letter correctly!"
+                putStrLn "Here is the word with the correctly guessed letter(s) revealed:"
+                let wordWithRevealedLetters = showCharInWord word char
+                putStrLn wordWithRevealedLetters
+            else
+                putStrLn "You have guessed incorrectly, sorry!"
+        return char)
+    guesses <- sequence (replicate 4 guessAChar)
+    print guesses
