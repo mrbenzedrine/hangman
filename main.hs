9a457a9409c270f11f43f09e7f97409bab55ffb8
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-05 21:47:23 +0000

    Move guessAChar out of the main function and define it to be a separate function instead

--- main.hs
+++ main.hs
@@ -6,19 +6,21 @@ main = do
     let wordCharsReplacedWithUnderscores = displayWordSpaced (showCharInWord word '_')
     putStrLn ("Here is the chosen word with all of its characters hidden: ")
     putStrLn wordCharsReplacedWithUnderscores
-    let guessAChar = (do
-        putStrLn "Please enter your guess for a letter in the chosen word:"
-        char <- getChar
-        print char
-        let isCharInWord = checkGuessedChar char word
-        if(isCharInWord)
-            then do
-                putStrLn "You have guessed a letter correctly!"
-                putStrLn "Here is the word with the correctly guessed letter(s) revealed:"
-                let wordWithRevealedLetters = showCharInWord word char
-                putStrLn wordWithRevealedLetters
-            else
-                putStrLn "You have guessed incorrectly, sorry!"
-        return char)
-    guesses <- sequence (replicate 4 guessAChar)
+    guesses <- sequence (replicate 4 (guessAChar word))
     print guesses
+
+guessAChar :: String -> IO Char
+guessAChar word = do
+    putStrLn "Please enter your guess for a letter in the chosen word:"
+    char <- getChar
+    print char
+    let isCharInWord = checkGuessedChar char word
+    if(isCharInWord)
+        then do
+            putStrLn "You have guessed a letter correctly!"
+            putStrLn "Here is the word with the correctly guessed letter(s) revealed:"
+            let wordWithRevealedLetters = showCharInWord word char
+            putStrLn wordWithRevealedLetters
+        else
+            putStrLn "You have guessed incorrectly, sorry!"
+    return char
