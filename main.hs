d17d13db1d4406b19e204a284ce9b905dcbc8da8
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-06 15:30:58 +0000

    Add an if else statement to guessAChar to ignore newline characters that are returned along with a character input as a consequence of hitting the enter key when using getChar

--- main.hs
+++ main.hs
@@ -14,21 +14,25 @@ guessAChar word listOfGuesses = do
     putStrLn "Please enter your guess for a letter in the chosen word:"
     char <- getChar
     print char
-    let isCharInWord = checkGuessedChar char word
-    if(isCharInWord)
-        then do
-            putStrLn "You have guessed a letter correctly!"
-            putStrLn "Here is the word with the correctly guessed letter(s) revealed:"
-            let wordWithRevealedLetters = showCharInWord word char
-            putStrLn wordWithRevealedLetters
-        else
-            putStrLn "You have guessed incorrectly, sorry!"
-    let wordWithAllCorrectlyGuessedLettersRevealed = showAllCorrectCharsInWord word (getListOfWordsWithCorrectGuesses (char:listOfGuesses) word)
-    putStrLn "Here is the word with all the correctly guessed so far letters revealed:"
-    putStrLn wordWithAllCorrectlyGuessedLettersRevealed
-    let isWordCompletelyGuessed = wordWithAllCorrectlyGuessedLettersRevealed == (displayWordSpaced word)
-    if(isWordCompletelyGuessed)
-        then do
-            putStrLn "You have successfully guessed the word, congratulations!"
-        else
-            guessAChar word (char:listOfGuesses)
+    if(char == '\n')
+        then
+            guessAChar word listOfGuesses
+        else do
+            let isCharInWord = checkGuessedChar char word
+            if(isCharInWord)
+                then do
+                    putStrLn "You have guessed a letter correctly!"
+                    putStrLn "Here is the word with the correctly guessed letter(s) revealed:"
+                    let wordWithRevealedLetters = showCharInWord word char
+                    putStrLn wordWithRevealedLetters
+                else
+                    putStrLn "You have guessed incorrectly, sorry!"
+            let wordWithAllCorrectlyGuessedLettersRevealed = showAllCorrectCharsInWord word (getListOfWordsWithCorrectGuesses (char:listOfGuesses) word)
+            putStrLn "Here is the word with all the correctly guessed so far letters revealed:"
+            putStrLn wordWithAllCorrectlyGuessedLettersRevealed
+            let isWordCompletelyGuessed = wordWithAllCorrectlyGuessedLettersRevealed == (displayWordSpaced word)
+            if(isWordCompletelyGuessed)
+                then do
+                    putStrLn "You have successfully guessed the word, congratulations!"
+                else
+                    guessAChar word (char:listOfGuesses)
