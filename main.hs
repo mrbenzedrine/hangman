98e27e95fe3e205a3cba14d059e1db876c8ae3cd
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-09 04:21:01 +0000

    Edit main.hs to handle multiple words (ie. phrases) correctly as the guessable string as well as single words

--- main.hs
+++ main.hs
@@ -2,13 +2,13 @@ import Hangman
 import Data.Char
 
 main = do
-    putStrLn "Please enter the word to be guessed"
+    putStrLn "Please enter the word or phrase to be guessed"
     word <- getLine
-    let wordCharsReplacedWithUnderscores = displayWordSpaced (showCharInWord word '_')
-    putStrLn ("Here is the chosen word with all of its characters hidden: ")
+    let wordCharsReplacedWithUnderscores = displayWordSpaced (showCharInWord word ' ')
+    putStrLn ("Here is the chosen word or phrase with all of its characters hidden: ")
     putStrLn wordCharsReplacedWithUnderscores
     let lowercaseWord = convertToLowerCase word
-    guessAChar lowercaseWord []
+    guessAChar lowercaseWord [' ']
 
 guessAChar :: String -> [Char] -> IO ()
 guessAChar word listOfGuesses = do
@@ -17,7 +17,7 @@ guessAChar word listOfGuesses = do
         maximumAllowedIncorrectGuesses = 5
         noOfIncorrectGuessesLeft = maximumAllowedIncorrectGuesses - (length incorrectGuesses)
     putStrLn ("You have made " ++ (show (length incorrectGuesses)) ++  " incorrect guess(es), so you are allowed to make only " ++ (show noOfIncorrectGuessesLeft) ++ " more incorrect guess(es)")
-    putStrLn "Please enter your guess for a letter in the chosen word:"
+    putStrLn "Please enter your guess for a letter in the chosen word or phrase:"
     char <- getChar
     if(char == '\n')
         then
@@ -37,19 +37,19 @@ guessAChar word listOfGuesses = do
                         else
                             putStrLn "You have guessed incorrectly, sorry!"
                     let wordWithAllCorrectlyGuessedLettersRevealed = showAllCorrectCharsInWord word (getListOfWordsWithCorrectGuesses (lowercaseChar:listOfGuesses) word)
-                    putStrLn "Here is the word with all the correctly guessed so far letters revealed:"
+                    putStrLn "Here is the word or phrase with all the correctly guessed so far letters revealed:"
                     putStrLn wordWithAllCorrectlyGuessedLettersRevealed
                     let isWordCompletelyGuessed = wordWithAllCorrectlyGuessedLettersRevealed == (displayWordSpaced word)
                         newIncorrectGuesses = getIncorrectGuesses word (lowercaseChar:listOfGuesses)
                     putStrLn ("Incorrect guesses so far are:" ++ newIncorrectGuesses)
                     if(isWordCompletelyGuessed)
                         then
-                            putStrLn "You have successfully guessed the word, congratulations!"
+                            putStrLn "You have successfully guessed the word or phrase, congratulations!"
                         else
                             if(length newIncorrectGuesses < maximumAllowedIncorrectGuesses)
                                 then
                                     guessAChar word (lowercaseChar:listOfGuesses)
                                 else do
                                     putStrLn "You have run out of guesses, sorry, you've lost!"
-                                    putStrLn "The word you were looking for was:"
+                                    putStrLn "The word or phrase you were looking for was:"
                                     putStrLn (displayWordSpaced word)
